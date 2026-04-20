24c95370e9c4d6b2a93b2b087ee086ce8703b78a
unknown <jovanvitohd@gmail.com>
2026-04-20 14:06:36 +0700

    Fix project name in vercel_app.py

--- vercel_app.py
+++ vercel_app.py
@@ -1,2 +1,2 @@
-from your_project_name.wsgi import application # Ganti 'your_project_name' dengan nama folder setting Django-mu
+from iot_backend.wsgi import application # Ganti 'your_project_name' dengan nama folder setting Django-mu
 app = application
